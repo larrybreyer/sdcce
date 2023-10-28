d4ff7534a257868721f45fa49992c0a3451ad3e6
Larry Breyer <larrybreyer@gmail.com>
2023-10-27 21:11:58 -0700

    daily tweaks

--- 2023/COMP692/module3/demos/path-module.js
+++ 2023/COMP692/module3/demos/path-module.js
@@ -1,23 +1,24 @@
 // For this script to work, ../package.json "type" must be "module"
-
 import path from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
-console.log(path.basename(__filename))
-
-console.log(path.dirname(__filename))
-
+// Base file name
 console.log(`Base file name: ${path.basename(__filename)}`)
 
+// Director name
 console.log(`Directory name: ${path.dirname(__filename)}`)
 
+// File extension
 console.log(`File extension: ${path.extname(__filename)}`)
 
+// Create path object
 console.log(path.parse(__filename))
 
 console.log(path.parse(__filename).base)
 
 console.log(path.join(__dirname, 'test','hello.html'))
+
+// vim: ts=2 et ai nu
---
+++ 2023/COMP692/module3/demos/url-module.js
@@ -0,0 +1,32 @@
+import url from 'url'
+
+const myUrl = new URL('http:/www.website.com:8000/hello.html?id=100&status=active')
+
+// Serialized URL
+console.log(myUrl.href)
+console.log(myUrl.toString())
+
+// Host (root domain)
+console.log(`Host: ${myUrl.host}`)
+
+// Hostname (does not get port)
+console.log(`Hostname: ${myUrl.hostname}`)
+
+// Pathname
+console.log(`Pathname: ${myUrl.pathname}`)
+
+// Serialized query
+console.log(`Query: ${myUrl.search}`)
+
+// Params object
+console.log(myUrl.searchParams)
+
+// Add custom params 
+myUrl.searchParams.append('abc','123')
+console.log(myUrl.searchParams)
+
+// Loop through params 
+myUrl.searchParams.forEach((value,name) => {
+  console.log(`${name}: ${value}`)
+})
+// vim: ts=2 et ai nu
