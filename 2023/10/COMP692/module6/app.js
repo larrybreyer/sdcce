6c3d781c2ead9602b4b00ea05a7b58eb887de3f5
Larry Breyer <larrybreyer@gmail.com>
2023-11-23 19:36:41 -0800

    daily tweaks

--- 2023/10/COMP692/module6/app.js
+++ 2023/10/COMP692/module6/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 const init = async () => {
   try {
     await connectDB(process.env.DB_URI)
-    console.log('Conencted to MongoDB')
+    console.log('Connected to MongoDB')
     app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
   } catch (err) {
     console.log(err)
