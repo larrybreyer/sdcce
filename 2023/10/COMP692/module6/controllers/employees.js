f17066e3a1f7914c667c843a38c404850a01610a
Larry Breyer <larrybreyer@gmail.com>
2023-11-24 20:56:38 -0800

    daily tweaks

--- 2023/10/COMP692/module6/controllers/employees.js
+++ 2023/10/COMP692/module6/controllers/employees.js
@@ -1,11 +1,27 @@
 import Employee from '../models/Employee.js'
 
-const getAllEmployees = (req, res) => {
-    res.send('Get all employees')
+const getAllEmployees = async (req, res) => {
+    try {
+        const employees = await Employee.find({})
+        res.status(200).json({employees})
+        // res.status(200).json({employees, count: employees.length})
+    } catch (err) {
+        res.status(500).json({msg: err})
+    }
 }
 
-const getEmployee = (req, res) => {
-    res.send('Get a single employee')
+const getEmployee = async (req, res) => {
+    try {
+        let {id:employeeId} = req.params
+        const employee = await Employee.findOne({_id: employeeId})
+        if (!employee) {
+            return  res.status(404).json(
+              {msg: `No employee with ID ${employeeId} found.`})
+        }
+        res.status(200).json({employee})
+    } catch (err) {
+        res.status(500).json({msg: err})
+    }
 }
 
 const createEmployee = async (req, res) => {
