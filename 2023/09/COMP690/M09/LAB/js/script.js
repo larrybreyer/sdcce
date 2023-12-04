768f2225fdba2834510be57ec7da4f7aa7b154fd
Larry Breyer <larrybreyer@gmail.com>
2023-12-03 20:40:49 -0800

    daily tweaks

--- 2023/09/COMP690/M09/LAB/js/script.js
+++ 2023/09/COMP690/M09/LAB/js/script.js
@@ -2,8 +2,15 @@
 const xhr = new XMLHttpRequest()
 xhr.onreadystatechange = () => {
     if(xhr.readyState == 4 && xhr.status == 200 ) {
-        console.log(xhr.responseXML)
+        let xmlDoc = xhr.responseXML
+        let team = xmlDoc.getElementsByTagName('teammember')
+        for (let i = 0; i < team.length; i++) {
+            document.body.innerHTML +=
+            xmlDoc.getElementsByTagName('name')[i].childNodes[0].nodeValue + '<br>' +   
+            xmlDoc.getElementsByTagName('title')[i].childNodes[0].nodeValue + '<br>' +
+            xmlDoc.getElementsByTagName('bio')[i].childNodes[0].nodeValue + '<br><br>'  
+        }
     }
 }
-xhr.open('Get','/data/team.xml')  
+xhr.open('Get','data/team.xml')  
 xhr.send()
