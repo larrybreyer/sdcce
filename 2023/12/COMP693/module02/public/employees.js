dd9004702929b5cae233cdab04954302033b04ac
Larry Breyer <larrybreyer@gmail.com>
2023-12-19 20:22:51 -0800

    daily tweaks

--- 2023/12/COMP693/module02/public/employees.js
+++ 2023/12/COMP693/module02/public/employees.js
@@ -15,14 +15,11 @@ const employees = [{
   dateHired: new Date('2015-06-03'),
   isEmployed: true
 }];
-
 class EmployeeFilter extends React.Component {
   render() {
     return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the employee filter.");
   }
-
 }
-
 class BorderWrap extends React.Component {
   render() {
     const borderedStyle = {
@@ -33,17 +30,13 @@ class BorderWrap extends React.Component {
       style: borderedStyle
     }, this.props.children);
   }
-
 }
-
 class EmployeeRow extends React.Component {
   render() {
     const employee = this.props.employee;
     return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", null, employee.name), /*#__PURE__*/React.createElement("td", null, employee.extension), /*#__PURE__*/React.createElement("td", null, employee.email), /*#__PURE__*/React.createElement("td", null, employee.title), /*#__PURE__*/React.createElement("td", null, employee.dateHired.toDateString()), /*#__PURE__*/React.createElement("td", null, employee.isEmployed ? 'Yes' : 'No'));
   }
-
 }
-
 class EmployeeTable extends React.Component {
   render() {
     const EmployeeRows = employees.map(employee => /*#__PURE__*/React.createElement(EmployeeRow, {
@@ -54,21 +47,17 @@ class EmployeeTable extends React.Component {
       className: "bordered-table"
     }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", null, "Name"), /*#__PURE__*/React.createElement("th", null, "Extension"), /*#__PURE__*/React.createElement("th", null, "Email"), /*#__PURE__*/React.createElement("th", null, "Title"), /*#__PURE__*/React.createElement("th", null, "Date Hired"), /*#__PURE__*/React.createElement("th", null, "Currently Employed?"))), /*#__PURE__*/React.createElement("tbody", null, EmployeeRows));
   }
-
 }
-
 class EmployeeAdd extends React.Component {
   render() {
     return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for a form to add an employee.");
   }
-
 }
-
 class EmployeeList extends React.Component {
   render() {
     return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(BorderWrap, null, /*#__PURE__*/React.createElement("h1", null, "Employee Management Application"), /*#__PURE__*/React.createElement(EmployeeFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeAdd, null)));
   }
-
 }
-
 ReactDOM.render( /*#__PURE__*/React.createElement(EmployeeList, null), document.getElementById('content'));
+
+// vim: ft=javascriptreact ai ts=2 et nu
