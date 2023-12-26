e956c7a7fed819c4c00500809806919eef375ab5
Larry Breyer <larrybreyer@gmail.com>
2023-12-25 19:52:22 -0800

    daily tweaks

--- 2023/12/COMP693/module04/public/employees.bundle.js
+++ 2023/12/COMP693/module04/public/employees.bundle.js
@@ -262,7 +262,7 @@ var EmployeeList = /*#__PURE__*/function (_React$Component) {
     }
   }]);
   return EmployeeList;
-}((react__WEBPACK_IMPORTED_MODULE_0___default().Component));
+}((react__WEBPACK_IMPORTED_MODULE_0___default().Component)); // vim: ai ts=2 et nu
 
 
 /***/ }),
