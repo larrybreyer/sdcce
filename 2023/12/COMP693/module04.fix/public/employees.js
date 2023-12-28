7fe707d151dbeefd611657e2b540b1eef9f0be25
Larry Breyer <larrybreyer@gmail.com>
2023-12-27 20:36:09 -0800

    daily tweaks

--- 2023/12/COMP693/module04.fix/public/employees.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-// import ReactDOM from 'react-dom'
-import { createRoot } from 'react-dom/client';
-import EmployeeList from './EmployeeList.jsx';
-const root = createRoot(document.getElementById('content'));
-root.render( /*#__PURE__*/React.createElement(React.StrictMode, null, /*#__PURE__*/React.createElement(EmployeeList, null)));
--- 2023/12/COMP693/module04.fix/src/EmployeeFilter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class EmployeeFilter extends React.Component {
-	render() {
-		return (
-			<div>This is a placeholder for the employee filter.</div>
-		)
-	}
-}
