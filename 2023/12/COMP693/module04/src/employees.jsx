017eca49efd01a8db5c3a525ef4c2614c9cc2416
Larry Breyer <larrybreyer@gmail.com>
2023-12-27 20:20:11 -0800

    daily tweaks

--- 2023/12/COMP693/module04/src/employees.jsx
+++ 2023/12/COMP693/module04/src/employees.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import EmployeeList from './EmployeeList.jsx'
 
+// Just adding a comment to see if it forces a recompile
+
 const root = createRoot(document.getElementById('content'))
 root.render(
   <React.StrictMode>
