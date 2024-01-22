b62f555beb6ed90e80de1ad6b6646ef917b48864
Larry Breyer <larrybreyer@gmail.com>
2024-01-22 13:04:04 -0800

    daily tweaks

--- 2023/09/COMP690/M13/Assigned/lab20/js/slideshow.js
+++ 2023/09/COMP690/M13/Assigned/lab20/js/slideshow.js
@@ -1,82 +1,82 @@
 // GET ELEMENT FROM THE DOM
-const $ = (id) => document.getElementById(id);
+const $ = (id) => document.getElementById(id)
 
 // MAIN SLIDESHOW OBJECT
 const slideshow = {
     timer: null,
-    nodes: { image: null, caption: null },
-    img: { cache: [], counter: 0 },
+    nodes: {image: null, caption: null},
+    img: {cache: [], counter: 0},
     speed: 2000, // Default speed
 
     // Public method to set the speed
     setSpeed: function (newSpeed) {
-        this.speed = newSpeed;
+        this.speed = newSpeed
         clearInterval(this.timer); // Clear the existing timer
         this.timer = setInterval(this.displayNextImage.bind(this), this.speed); // Set a new timer with the updated speed
     },
 
     // Public method to get the speed
     getSpeed: function () {
-        return this.speed;
+        return this.speed
     },
 
     loadImages: function (slides) {
-        let image;
+        let image
         for (let i = 0; i < slides.length; i++) {
-            image = new Image();
-            image.src = slides[i].href;
-            image.title = slides[i].title;
-            this.img.cache.push(image);
+            image = new Image()
+            image.src = slides[i].href
+            image.title = slides[i].title
+            this.img.cache.push(image)
         }
-        return this;
+        return this
     },
     startSlideShow: function (image, caption) {
-        this.nodes.image = image;
-        this.nodes.caption = caption;
-        this.timer = setInterval(this.displayNextImage.bind(this), this.speed);
-        return this;
+        this.nodes.image = image
+        this.nodes.caption = caption
+        this.timer = setInterval(this.displayNextImage.bind(this), this.speed)
+        return this
     },
     displayNextImage: function () {
         if (this.img.counter === this.img.cache.length) {
-            this.img.counter = 0;
+            this.img.counter = 0
         } else {
-            this.img.counter++;
+            this.img.counter++
         }
-        let image = this.img.cache[this.img.counter];
-        this.nodes.image.src = image.src;
-        this.nodes.caption.innerHTML = image.title;
+        let image = this.img.cache[this.img.counter]
+        this.nodes.image.src = image.src
+        this.nodes.caption.innerHTML = image.title
     },
-};
+}
 
 // WIRE UP SLIDESHOW ON LOAD
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
     // ARRAY OF SLIDE FOR SLIDESHOW
     const slides = [
-        { href: "images/backpack.jpg", title: "He likes to backpack in the Sierras." },
-        { href: "images/boat.jpg", title: "He loves his wake boat." },
-        { href: "images/camaro.jpg", title: "He loves his 67 Camaro." },
-        { href: "images/punk.jpg", title: "He used to be in a punk band." },
-        { href: "images/race.jpg", title: "He loves to obstacle course race." },
-        { href: "images/wakeboard.jpg", title: "He loves to wakeboard." },
-        { href: "images/wakesurf.jpg", title: "He also loves to wakesurf." },
-    ];
+        {href: 'images/backpack.jpg', title: 'He likes to backpack in the Sierras.'},
+        {href: 'images/boat.jpg', title: 'He loves his wake boat.'},
+        {href: 'images/camaro.jpg', title: 'He loves his 67 Camaro.'},
+        {href: 'images/punk.jpg', title: 'He used to be in a punk band.'},
+        {href: 'images/race.jpg', title: 'He loves to obstacle course race.'},
+        {href: 'images/wakeboard.jpg', title: 'He loves to wakeboard.'},
+        {href: 'images/wakesurf.jpg', title: 'He also loves to wakesurf.'}
+    ]
 
     // BEGIN THE SLIDE SHOW
-    slideshow.loadImages(slides).startSlideShow($("image"), $("caption"));
+    slideshow.loadImages(slides).startSlideShow($('image'), $('caption'))
 
-    // Get the "Set Speed" button element
-    const setSpeedButton = $('setSpeedButton');
+    // Get the 'Set Speed' button element
+    const setSpeedButton = $('setSpeedButton')
 
-    // Add a click event listener to the "Set Speed" button
+    // Add a click event listener to the 'Set Speed' button
     setSpeedButton.addEventListener('click', () => {
-        const newSpeed = prompt("Enter the new speed (in milliseconds):");
+        const newSpeed = prompt('Enter the new speed (in milliseconds):')
         if (newSpeed !== null) {
-            const parsedSpeed = parseInt(newSpeed, 10);
+            const parsedSpeed = parseInt(newSpeed, 10)
             if (!isNaN(parsedSpeed) && parsedSpeed > 0) {
-                slideshow.setSpeed(parsedSpeed);
+                slideshow.setSpeed(parsedSpeed)
             } else {
-                alert("Invalid input. Please enter a positive number.");
+                alert('Invalid input. Please enter a positive number.')
             }
         }
-    });
-});
+    })
+})
