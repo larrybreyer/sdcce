48d10e040a6d47847562c38f7791b9b6c419b879
Larry Breyer <larrybreyer@gmail.com>
2024-02-18 15:54:06 -0800

    daily tweaks

---
+++ 2024S1/COMP690/M2/Assignment01/js/script.js
@@ -0,0 +1,56 @@
+
+// STEP 1
+// let someMonth               
+// function theMonth()            
+// let currentMonth               
+// let summerMonth             
+// let MyLibraryFunction         
+//
+// STEP 2
+3.141592653
+'Hello World!'
+true
+null
+//
+// STEP 3
+let anExpression = 3 * (4 / 5) + 6
+const foo = ["one", "two", "three"];
+//
+// STEP 4
+//
+// STEP 5
+//
+// STEP 6
+//
+// STEP 7
+//
+// STEP 8
+//
+// STEP 9
+//
+// STEP 10
+//
+// STEP 11
+//
+// STEP 12
+//
+// STEP 13
+//
+// STEP 14
+//
+// STEP 15
+//
+// STEP 16
+//
+// STEP 17
+//
+// STEP 18
+//
+// STEP 19
+//
+// STEP 20
+//
+// STEP 21
+//
+// STEP 22
+//
