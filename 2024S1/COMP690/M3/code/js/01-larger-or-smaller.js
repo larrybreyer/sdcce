e9612f6e9dac23be0a6bc407fc4c3dc8ef45691b
Larry Breyer <larrybreyer@gmail.com>
2024-02-27 16:53:50 -0800

    daily tweaks

--- 2024S1/COMP690/M3/code/js/01-larger-or-smaller.js
+++ 2024S1/COMP690/M3/code/js/01-larger-or-smaller.js
@@ -1,7 +1,11 @@
-let number1 = prompt("Please enter a number");
-let number2 = prompt("Please enter another number");
-if ( number1 >= number2 ) {
-  document.write(number1);
-} else {
-  document.write(number2);
-}
+"use strict";
+document.ready( () => {
+  let number1 = prompt("Please enter a number");
+  let number2 = prompt("Please enter another number");
+  if ( number1 >= number2 ) {
+    document.write("<h1>" + number1.toString() + "</h1>");
+  } else {
+    document.write("<h1>" + number2.toString() + "</h1>");
+    //  document.write(number2);
+  }
+});
