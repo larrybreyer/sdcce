73c9e65d38c2de19e5232371361cb090dfef72f1
Larry Breyer <larrybreyer@gmail.com>
2024-04-22 15:30:33 -0700

    daily tweaks

---
+++ 2024S1/COMP690/M09/Assignment/js/modules/init.js
@@ -0,0 +1,19 @@
+// imported by js/script.js
+export async function fetchUsers() {
+    let users = []
+    try {
+        const response = await fetch('./data/employees.json'); 
+        users = await response.json();
+          //  for (let user of users) {
+          //      document.body.innerHTML += 
+          //               `${user.name},${user.email}<br>`
+          //  } 
+    } catch (error) {
+        console.error(error);
+    }
+    console.log(users)
+    return users;
+
+}
+// fetchUsers();
+// vim: ai ts=4 et nu
