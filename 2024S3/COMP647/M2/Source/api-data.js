241e54a3e4b1ee5efc9d57eca62224759caf43d6
Larry Breyer <larrybreyer@gmail.com>
2024-11-15 19:08:54 -0800

    daily tweaks

--- 2024S3/COMP647/M2/Source/api-data.js
+++ 2024S3/COMP647/M2/Source/api-data.js
@@ -1,3 +1,7 @@
-let ts = "1681802982683";
-let publicKey = "";
-let hashVal = "";
+// See ~/breyer/log/2024/11/20241109MAD for how keys were obtained
+let ts = "1731717994279";
+let privateKey="c34fe52fa47e9c12ff3b049d8678e8579784cfc9"
+let publicKey = "7c2f86ecb0277289b9728500180b2035";
+// md5 -s ts+privateKey+publicKey
+let hashVal = "2fde6b4390be896ab92846ecf6ae3568";
+
